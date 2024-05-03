1376cb61be6e28fa18f10257767ef5e3913871a1
kjasamil <kamil200202@gmail.com>
2024-05-03 20:39:46 +0200

    Improved encrypting and decrypting in binary mode

--- knapsack/algorithm.py
+++ knapsack/algorithm.py
@@ -1,7 +1,7 @@
-import numpy as np
 import sympy as sp
 import math as m
 import random as ran
+import struct
 
 
 def get_bit(byte, index):
@@ -47,7 +47,7 @@ class Knapsack:
         self.inv_w = int(sp.mod_inverse(self.w, self.m))
         elements_sum = 0
         for i in range(8):
-            element = ran.randint(2**((i*2)+7), 2**((i*2)+8)-1)
+            element = ran.randint(2**((i*2)+10), 2**((i*2)+11)-1)
             while element < elements_sum:
                 element = ran.randint(2 ** ((i * 2) + 7), 2 ** ((i * 2) + 8)-1)
             elements_sum += element
@@ -62,6 +62,7 @@ class Knapsack:
         if is_text:
             plain_text = plain_text.encode("iso-8859-2")
         cipher = []
+        cipher_bytes = bytearray()
         for i in range(len(plain_text)):
             partial_sum = 0
             byte = plain_text[i]
@@ -70,35 +71,60 @@ class Knapsack:
                 if bit:
                     partial_sum += self.a_prim[j]
             cipher.append(partial_sum)
-        cipher = ",".join([hex(x)[2:] for x in cipher])
-        cipher = cipher.encode("iso-8859-2")
-        return cipher
+        for num in cipher:
+            cipher_bytes.extend(struct.pack("q", num))
+        if is_text:
+            hex_cipher = ['{:016x}'.format(num) for num in cipher]
+            hex_cipher = "".join(hex_cipher)
+            return hex_cipher
+        return cipher_bytes
 
     def decrypt(self, cipher_text, is_text):
         plain_text = bytearray()
-        cipher_text = cipher_text.decode("iso-8859-2")
-        cipher_text = cipher_text.split(",")
-        cipher_text = [int(hex_str, 16) for hex_str in cipher_text]
-        for i in range(len(cipher_text)):
+        if is_text:
+            cipher_string = [cipher_text[i:i + 16] for i in range(0, len(cipher_text), 16)]
+            cipher = [int(hex_str, 16) for hex_str in cipher_string]
+        else:
+            cipher = []
+            for i in range(0, len(cipher_text), 8):
+                num = struct.unpack("q", cipher_text[i:i + 8])[0]
+                cipher.append(num)
+        for i in range(len(cipher)):
             plain_text.append(0)
-            cipher_text[i] *= self.inv_w
-            cipher_text[i] %= self.m
+            cipher[i] *= self.inv_w
+            cipher[i] %= self.m
             for j in range(8):
-                if cipher_text[i] >= self.a[7-j]:
+                if cipher[i] >= self.a[7-j]:
                     plain_text[i] = set_bit(plain_text[i], True, 7-j)
-                    cipher_text[i] -= self.a[7-j]
+                    cipher[i] -= self.a[7-j]
         if is_text:
             plain_text = plain_text.decode("iso-8859-2")
         return plain_text
 
 # TESTING PURPOSES
+# TEXT ENCRYPTION
 
 
-knap = Knapsack()
-print("klucz prywatny", knap.a_string)
-print("Klucz jawny", knap.a_prim_string)
-print("Wartość m", knap.m_string)
-print("Wartość w", knap.w_string)
-encrypted = knap.encrypt("Gżegżółka", True)
-print("Kryptogram", encrypted)
-print("Odszyfrowane:", knap.decrypt(encrypted, True))
+# knap = Knapsack()
+# print("klucz prywatny", knap.a_string)
+# print("Klucz jawny", knap.a_prim_string)
+# print("Wartość m", knap.m_string)
+# print("Wartość w", knap.w_string)
+# encrypted = knap.encrypt("Gżegżółka", True)
+# print("Kryptogram", encrypted)
+# print("Odszyfrowane:", knap.decrypt(encrypted, True))
+#
+# # PICTURE ENCRYPTION
+#
+# with open("obraz.png", "rb") as file:
+#     data = file.read()
+#
+# encrypted_picture = knap.encrypt(data, False)
+#
+# with open("obraz_zaszyfrowany.png", "wb") as file:
+#     file.write(encrypted_picture)
+#
+# decrypted_picture = knap.decrypt(encrypted_picture, False)
+#
+# with open("obraz_odszyfrowany.png", "wb") as file:
+#     file.write(decrypted_picture)
